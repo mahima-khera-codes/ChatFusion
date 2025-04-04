9dda358dab1dc03792f42c147c544e7e6a66c214
mahima-khera-codes <mahima.excel2011@gmail.com>
2025-04-04 20:26:35 +0200

    made meaningful changes to improve the project

---
+++ main_fluid.py
@@ -0,0 +1,333 @@
+import argparse
+import datetime
+import numpy as np
+import os
+import time
+from pathlib import Path
+
+import torch
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from util.crop import center_crop_arr
+import util.misc as misc
+from util.misc import NativeScalerWithGradNormCount as NativeScaler
+from util.loader import CacheDataset, ImageCaptionDataset # TxtCacheDataset, CacheCaptionDataset, TxtPathDataset, ImageCaptionDataset, LAIONJSONDataset
+
+
+from diffusers.models import AutoencoderKL
+from models import fluid_arbitrary
+from models.utils import T5_Embedding
+from engine_fluid import train_one_epoch, evaluate
+import copy
+import wandb
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('LightGen pre-training', add_help=False)
+    parser.add_argument('--batch_size', default=256, type=int,
+                        help='Batch size per GPU (effective batch size is batch_size * # gpus')
+    parser.add_argument('--epochs', default=400, type=int)
+
+    # Model parameters
+    parser.add_argument('--model', default='mar_large', type=str, metavar='MODEL',
+                        help='Name of model to train')
+    parser.add_argument('--model_path', default='checkpoint-last.pth', type=str, metavar='MODEL',
+                        help='Name of model to train')
+                        
+    # VAE parameters
+    parser.add_argument('--img_size', default=256, type=int,
+                        help='images input size')
+    parser.add_argument('--vae_path', default="", type=str,
+                        help='images input size')
+    parser.add_argument('--vae_embed_dim', default=16, type=int,
+                        help='vae output embedding dimension')
+    parser.add_argument('--vae_stride', default=16, type=int,
+                        help='tokenizer stride, default use KL16')
+    parser.add_argument('--patch_size', default=1, type=int,
+                        help='number of tokens to group as a patch.')
+    
+    # Text embedding
+    parser.add_argument('--mt5_cache_dir', type=str, default='')
+    parser.add_argument('--max_length', type=int, default=512)
+
+    # Generation parameters
+    parser.add_argument('--num_iter', default=64, type=int,
+                        help='number of autoregressive iterations to generate an image')
+    parser.add_argument('--cfg', default=1.0, type=float, help="classifier-free guidance")
+    parser.add_argument('--cfg_schedule', default="linear", type=str)
+    parser.add_argument('--text_drop_prob', default=0.1, type=float)
+    parser.add_argument('--eval_freq', type=int, default=40, help='evaluation frequency')
+    parser.add_argument('--save_last_freq', type=int, default=5, help='save last frequency')
+
+    # Optimizer parameters
+    parser.add_argument('--weight_decay', type=float, default=0.02,
+                        help='weight decay (default: 0.02)')
+
+    parser.add_argument('--grad_checkpointing', action='store_true')
+    parser.add_argument('--lr', type=float, default=None, metavar='LR',
+                        help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=1e-4, metavar='LR',
+                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--min_lr', type=float, default=0., metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0')
+    parser.add_argument('--lr_schedule', type=str, default='constant',
+                        help='learning rate schedule')
+    parser.add_argument('--warmup_epochs', type=int, default=100, metavar='N',
+                        help='epochs to warmup LR')
+    parser.add_argument('--ema_rate', default=0.9999, type=float)
+
+    # Fluid params
+    parser.add_argument('--mask_ratio_min', type=float, default=0.7,
+                        help='Minimum mask ratio')
+    parser.add_argument('--grad_clip', type=float, default=3.0,
+                        help='Gradient clip')
+    parser.add_argument('--attn_dropout', type=float, default=0.1,
+                        help='attention dropout')
+    parser.add_argument('--proj_dropout', type=float, default=0.1,
+                        help='projection dropout')
+
+    # Diffusion Loss params
+    parser.add_argument('--diffloss_d', type=int, default=12)
+    parser.add_argument('--diffloss_w', type=int, default=1536)
+    parser.add_argument('--num_sampling_steps', type=str, default="100")
+    parser.add_argument('--diffusion_batch_mul', type=int, default=1)
+    parser.add_argument('--temperature', default=1.0, type=float, help='diffusion loss sampling temperature')
+
+    # Dataset parameters
+    parser.add_argument('--data_path', default='', type=str,
+                        help='dataset path')
+    parser.add_argument('--json_path', default='./data/data.json', type=str,
+                        help='dataset path')
+                        
+    parser.add_argument('--cache_folder', default='./data/data.json', type=str,
+                        help='dataset path')
+
+    parser.add_argument('--output_dir', default='./output_dir',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default='./output_dir',
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=1, type=int)
+    parser.add_argument('--resume', default='',
+                        help='resume from checkpoint')
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.set_defaults(pin_mem=True)
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    # caching latents
+    parser.add_argument('--use_cached', action='store_true', dest='use_cached',
+                        help='Use cached latents')
+    # parser.set_defaults(use_cached=False)
+    parser.add_argument('--cached_path', default='', help='path to cached latents')
+
+    # finetune 512px or high resolution
+    parser.add_argument('--finetune', action='store_true', 
+                        help='whether finetune hq or not')
+    parser.add_argument('--finetune_path', default='', help='path to ckpt')
+
+
+    return parser
+
+
+def main(args):
+    misc.init_distributed_mode(args)
+
+    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
+    print("{}".format(args).replace(', ', ',\n'))
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    cudnn.benchmark = True
+
+    num_tasks = misc.get_world_size()
+    global_rank = misc.get_rank()
+
+    if global_rank == 0 and args.log_dir is not None:
+        os.makedirs(args.log_dir, exist_ok=True)
+        log_writer = SummaryWriter(log_dir=args.log_dir)
+    else:
+        log_writer = None
+    
+    # Initialize WandB
+    if global_rank == 0:  # Ensure WandB is only initialized by the main process
+        wandb.init(
+            project="fluid_512",  # Replace with your WandB project name
+            name=f"512_{args.model}",  # Run name
+            config=vars(args),  # Log all argparse arguments
+            dir=args.output_dir  # Directory to save WandB logs
+        )
+
+    # augmentation following DiT and ADM
+    transform_train = transforms.Compose([
+        transforms.Lambda(lambda pil_image: center_crop_arr(pil_image, args.img_size)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+    # dataset_train = ImageCaptionDataset(folder_path=args.data_path, transform=transform_train)
+
+    # dataset_train = TxtCacheDataset(txt_file=args.json_path, root_dir=args.data_path, cache_folder=args.cache_folder, transform=transform_train)
+    dataset_train = CacheDataset(txt_file=args.json_path, cache_folder=args.cache_folder, text_folder=args.data_path)
+    print(dataset_train)
+
+    sampler_train = torch.utils.data.DistributedSampler(
+        dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
+    )
+    print("Sampler_train = %s" % str(sampler_train))
+
+    data_loader_train = torch.utils.data.DataLoader(
+        dataset_train, sampler=sampler_train,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
+        drop_last=True,
+    )
+
+    # define the vae and mar model
+    vae = AutoencoderKL.from_pretrained(os.path.join(args.vae_path, "vae")).cuda().eval()
+    for param in vae.parameters():
+        param.requires_grad = False
+
+    # t5_emb = T5_Embedding(args.mt5_cache_dir, args.mt5_cache_dir, args.max_length).cuda()
+
+    model = fluid_arbitrary.__dict__[args.model](
+        img_size=args.img_size,
+        vae_stride=args.vae_stride,
+        patch_size=args.patch_size,
+        vae_embed_dim=args.vae_embed_dim,
+        mask_ratio_min=args.mask_ratio_min,
+        text_drop_prob=args.text_drop_prob,
+        attn_dropout=args.attn_dropout,
+        proj_dropout=args.proj_dropout,
+        diffloss_d=args.diffloss_d,
+        diffloss_w=args.diffloss_w,
+        num_sampling_steps=args.num_sampling_steps,
+        diffusion_batch_mul=args.diffusion_batch_mul,
+        grad_checkpointing=args.grad_checkpointing,
+        max_length=args.max_length
+    )
+
+    print("Model = %s" % str(model))
+    # following timm: set wd as 0 for bias and norm layers
+    n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print("Number of trainable parameters: {}M".format(n_params / 1e6))
+
+    model.to(device)
+    model_without_ddp = model
+
+    eff_batch_size = args.batch_size * misc.get_world_size()
+
+    if args.lr is None:  # only base_lr is specified
+        args.lr = args.blr * eff_batch_size / 256
+
+    print("base lr: %.2e" % (args.lr * 256 / eff_batch_size))
+    print("actual lr: %.2e" % args.lr)
+    print("effective batch size: %d" % eff_batch_size)
+
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+
+    # no weight decay on bias, norm layers, and diffloss MLP
+    param_groups = misc.add_weight_decay(model_without_ddp, args.weight_decay)
+    optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
+    print(optimizer)
+    loss_scaler = NativeScaler()
+
+    if args.finetune and not os.path.exists(os.path.join(args.resume, "checkpoint-last.pth")):
+        print("Finetune from scratch")
+        checkpoint = torch.load(args.finetune_path, map_location='cpu')
+        model_without_ddp.load_state_dict(checkpoint['model'])
+        model_params = list(model_without_ddp.parameters())
+        ema_state_dict = checkpoint['model_ema']
+        ema_params = [ema_state_dict[name].cuda() for name, _ in model_without_ddp.named_parameters()]
+        print("Start from checkpoint %s" % args.resume)
+
+    # resume training
+    if args.resume and os.path.exists(os.path.join(args.resume, "checkpoint-last.pth")):
+        checkpoint = torch.load(os.path.join(args.resume, "checkpoint-last.pth"), map_location='cpu')
+        model_without_ddp.load_state_dict(checkpoint['model'])
+        model_params = list(model_without_ddp.parameters())
+        ema_state_dict = checkpoint['model_ema']
+        ema_params = [ema_state_dict[name].cuda() for name, _ in model_without_ddp.named_parameters()]
+        print("Resume checkpoint %s" % args.resume)
+
+        if 'optimizer' in checkpoint and 'epoch' in checkpoint:
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            args.start_epoch = checkpoint['epoch'] + 1
+            if 'scaler' in checkpoint:
+                loss_scaler.load_state_dict(checkpoint['scaler'])
+            print("With optim & sched!")
+        del checkpoint
+    else:
+        model_params = list(model_without_ddp.parameters())
+        ema_params = copy.deepcopy(model_params)
+        print("Training from scratch")
+
+    # training
+    print(f"Start training for {args.epochs} epochs")
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            data_loader_train.sampler.set_epoch(epoch)
+
+        # train_one_epoch(
+        #     model, vae, t5_emb, model_params, ema_params,
+        #     data_loader_train,
+        #     optimizer, device, epoch, loss_scaler,
+        #     log_writer=log_writer, wandb=wandb, global_rank=global_rank,
+        #     args=args
+        # )
+        train_one_epoch(
+            model, vae, model_params, ema_params,
+            data_loader_train,
+            optimizer, device, epoch, loss_scaler,
+            log_writer=log_writer, wandb=wandb, global_rank=global_rank,
+            args=args
+        )
+
+        # save checkpoint
+        if epoch % args.save_last_freq == 0 or epoch + 1 == args.epochs:
+            # misc.save_model(args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+            #                 loss_scaler=loss_scaler, epoch=epoch, ema_params=ema_params, epoch_name="last")
+            misc.save_model(args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+                            loss_scaler=loss_scaler, epoch=epoch, ema_params=ema_params, epoch_name=str(epoch))
+
+        if misc.is_main_process():
+            if log_writer is not None:
+                log_writer.flush()
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
+
+
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    args.log_dir = args.output_dir
+    main(args)
